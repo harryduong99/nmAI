a6c1094989d1e0cacb44fd566637312922f412a6
duongnam <duongminhnam99@gmail.com>
2019-12-03 05:28:18 +0700

    udpate full api

--- app/views.py
+++ app/views.py
@@ -5,11 +5,15 @@ from werkzeug.utils import secure_filename
 import sys
 import os
 from pprint import pprint
-# from imageai.Detection import ObjectDetection
-# from .core.classify_dog_breed import predict
+import base64
 
-app.config["IMAGE_UPLOADS"] = "/home/duongnam/Desktop/python-service/app/app/core/detect_object/input_data"
+import requests
+import cv2
+import numpy as np
+from keras.models import load_model
+
 app.config["IMAGE_STATIC"] = 'app/static/img/object_detected'
+app.config["MODEL"] = 'app/core'
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
 app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
 
@@ -24,85 +28,6 @@ def about():
     <p>This is a lovely little paragraph</p>
     <code>Flask is <em>awesome</em></code>
     """
-
-@app.route("/jinja")
-def jinja():
-
-    # Strings
-    my_name = "Julian"
-
-    # Integers
-    my_age = 30
-
-    # Lists
-    langs = ["Python", "JavaScript", "Bash", "Ruby", "C", "Rust"]
-
-    # Dictionaries
-    friends = {
-        "Tony": 43,
-        "Cody": 28,
-        "Amy": 26,
-        "Clarissa": 23,
-        "Wendell": 39
-    }
-
-    # Tuples
-    colors = ("Red", "Blue")
-
-    # Booleans
-    cool = True
-
-    # Classes
-    class GitRemote:
-        def __init__(self, name, description, domain):
-            self.name = name
-            self.description = description 
-            self.domain = domain
-
-        def pull(self):
-            return f"Pulling repo '{self.name}'"
-
-        def clone(self, repo):
-            return f"Cloning into {repo}"
-
-    my_remote = GitRemote(
-        name="Learning Flask",
-        description="Learn the Flask web framework for Python",
-        domain="https://github.com/Julian-Nash/learning-flask.git"
-    )
-
-    # Functions
-    def repeat(x, qty=1):
-        return x * qty
-
-    return render_template(
-        "public/jinja.html", my_name=my_name, my_age=my_age, langs=langs,
-        friends=friends, colors=colors, cool=cool, GitRemote=GitRemote, 
-        my_remote=my_remote, repeat=repeat
-    )
-
-# @app.route("/sign-up", methods=["GET", "POST"])
-# def sign_up():
-
-#     if request.method == "POST":
-#         # print(request.url)
-#         username = request.form.get("username")
-#         email = request.form.get("email")
-#         password = request.form.get("password")
-
-#         # Alternatively
-
-#         # username = request.form["username"]
-#         # email = request.form["email"]
-#         # password = request.form["password"]
-
-#         # return redirect(request.url)
-#         return jsonify({'name': username,
-#                         'email': email,
-#                         'password': password
-#                         })
-#     else:
-#         return render_template("public/sign_up.html")
 
 def allowed_image(filename):
     
@@ -126,60 +51,59 @@ def allowed_image_filesize(filesize):
 
 
 # @app.route("/upload-image", methods=["GET", "POST"])
-# def upload_image():
+@app.route("/upload-image", methods=["POST"])
+def upload_image():
+    
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            execution_path = os.getcwd()
 
-#     if request.method == "POST":
-#         if request.files:
-#             if "filesize" in request.cookies:
-#                 if not allowed_image_filesize(request.cookies["filesize"]):
-#                     print("Filesize exceeded maximum limit")
-#                     return redirect(request.url)
-
-#                 image = request.files["image"]
-
-#                 if image.filename == "":
-#                     print("No filename")
-#                     return redirect(request.url)
-
-#                 if allowed_image(image.filename):
-
-#                     execution_path = os.getcwd()
-
-#                     filename = secure_filename(image.filename)
-#                     # image_path = os.path.join(app.config["IMAGE_UPLOADS"], filename)
-#                     uploaded_path = os.path.join(execution_path, app.config["IMAGE_STATIC"], 'input', filename)
-#                     image.save(uploaded_path)
-
-#                     detector = ObjectDetection()
-#                     detector.setModelTypeAsRetinaNet()
-#                     detector.setModelPath( os.path.join(execution_path , 'app/core/detect_object/detect_model/', "resnet50_coco_best_v2.0.1.h5"))
-#                     detector.loadModel()
-
-#                     detections = detector.detectObjectsFromImage(input_image=uploaded_path, 
-#                     output_image_path=os.path.join(execution_path, app.config["IMAGE_STATIC"], 'output/' + filename ), extract_detected_objects=True)
-
-#                     accepted_dogs = filter_detect_dog(detections)
-#                     # return jsonify(detections)
-#                     dog_predictor = predict.DogBreedPrediction(accepted_dogs)
-#                     results = dog_predictor.predict()
-
-#                     img_public_path = os.path.join('static', 'img/object_detected')
-#                     data_final = {}
-#                     for i in range(len(accepted_dogs)):
-#                         data_final[os.path.join(img_public_path, accepted_dogs[i][73:])] = results[i]
-#                     show_uploaded_path = uploaded_path[46:]
-#                     return render_template("public/result_detect.html", results=data_final, show_uploaded_path=show_uploaded_path )
-#                 else:
-#                     print("That file extension is not allowed")
-#                     return redirect(request.url)
-
-#     return render_template("public/upload_image.html")
+            model_path = os.path.join(execution_path, app.config["MODEL"], 'model.h5')
+            filename = secure_filename(image.filename)
+            uploaded_path = os.path.join(execution_path, app.config["IMAGE_STATIC"], 'input', filename)
+            image.save(uploaded_path)
+            output_path_img_temp = os.path.join(execution_path, app.config["IMAGE_STATIC"], 'output', filename)
 
 
-# def filter_detect_dog(detection):
-#     result = []
-#     for i, image in enumerate(detection[0]):
-#         if image["percentage_probability"] > 90 and image["name"] == "dog":
-#             result.append(detection[1][i])
-#     return result
-                
+            model = load_model(model_path, compile = False)
+            img = cv2.imread(uploaded_path)
+
+            mapping = ['0', '1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+            gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            res_read = ''
+
+            ret, thresh = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY_INV)
+
+            contours, hier = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            sorted_ctrs = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
+
+            for i, crt in enumerate(sorted_ctrs):
+                x, y, w, h = cv2.boundingRect(crt)
+                if 500 > w > 10 and 500 > h > 50:
+                    roi = img[y-20:y+h+20, x-20:x+w+20]
+                    cv2.imwrite(output_path_img_temp, roi)
+                    cropped_img = cv2.imread(output_path_img_temp, 0)
+                    blur = cv2.GaussianBlur(cropped_img, (5, 5), 0)
+                    ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                    th3 = cv2.subtract(255, th3)
+                    pred_img = th3
+                    pred_img = cv2.resize(pred_img, (28, 28))
+                    pred_img = pred_img / 255.0
+                    pred_img = pred_img.reshape(1, 784)
+                    prediction = mapping[model.predict_classes(pred_img)[0]]
+                    res_read = res_read + prediction
+                    # cv2.imshow('charachter'+str(i), roi)
+                    # cv2.waitKey(0)
+            
+            query = {'q': res_read}
+            req = requests.get('https://nhqt-dict.herokuapp.com/api/e_search', params=query)
+        
+            return jsonify({
+                'result': req.text,
+                'read_text': res_read
+                }), 201
+
+    return jsonify({
+        'result': 'error',
+        'read_text': ''}), 201   
