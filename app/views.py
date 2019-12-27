7ed70e2b92d7041214a371eb75ea12256f3c5671
nguyenthucquanghung <nguyenthucquanghung@gmail.com>
2019-12-27 16:07:02 +0700

    modify model

--- app/views.py
+++ app/views.py
@@ -14,15 +14,19 @@ import http.client
 from cv2 import cv2
 import numpy as np
 from keras.models import load_model
+import keras.backend.tensorflow_backend as tb
+
 
 app.config["IMAGE_STATIC"] = 'app/static/img/object_detected'
 app.config["MODEL"] = 'app/core'
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
 app.config["MAX_IMAGE_FILESIZE"] = 0.5 * 1024 * 1024
 
+
 @app.route("/")
 def index():
     return render_template("public/index.html")
+
 
 @app.route("/about")
 def about():
@@ -31,6 +35,7 @@ def about():
     <p>This is a lovely little paragraph</p>
     <code>Flask is <em>awesome</em></code>
     """
+
 
 def allowed_image(filename):
     
@@ -58,17 +63,16 @@ def upload_image():
     
     if request.method == "POST":
         if request.files:
+            tb._SYMBOLIC_SCOPE.value = True
             image = request.files["image"]
             execution_path = os.getcwd()
 
-            # model_path = os.path.join(execution_path, app.config["MODEL"], 'model_update.h5')
-            model_path = os.path.join(execution_path, app.config["MODEL"], 'model.h5')
+            model_path = os.path.join(execution_path, app.config["MODEL"], 'model_update.h5')
+            # model_path = os.path.join(execution_path, app.config["MODEL"], 'model.h5')
             filename = secure_filename(image.filename)
             uploaded_path = os.path.join(execution_path, app.config["IMAGE_STATIC"], 'input', filename)
             image.save(uploaded_path)
             output_path_img_temp = os.path.join(execution_path, app.config["IMAGE_STATIC"], 'output', filename)
-
-
             model = load_model(model_path, compile = False)
             img = cv2.imread(uploaded_path)
             mapping = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -84,19 +88,7 @@ def upload_image():
             for i, crt in enumerate(sorted_ctrs):
                 x, y, w, h = cv2.boundingRect(crt)
                 if 500 > w > 10 and 500 > h > 50 :
-                    roi = img[y-20:y+h+20, x-20:x+w+20]
-                    cv2.imwrite(output_path_img_temp, roi)
-                    cropped_img = cv2.imread(output_path_img_temp, 0)
-                    blur = cv2.GaussianBlur(cropped_img, (5, 5), 0)
-                    ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-                    th3 = cv2.subtract(255, th3)
-                    pred_img = th3
-                    pred_img = cv2.resize(pred_img, (28, 28))
-                    pred_img = pred_img / 255.0
-                    pred_img = pred_img.reshape(1, 784)
-                    prediction = mapping[model.predict_classes(pred_img)[0]]
-
-                    # roi = gray_img[y-50:y+h+50, x-50:x+w+50]
+                    # roi = img[y-20:y+h+20, x-20:x+w+20]
                     # cv2.imwrite(output_path_img_temp, roi)
                     # cropped_img = cv2.imread(output_path_img_temp, 0)
                     # blur = cv2.GaussianBlur(cropped_img, (5, 5), 0)
@@ -104,8 +96,20 @@ def upload_image():
                     # th3 = cv2.subtract(255, th3)
                     # pred_img = th3
                     # pred_img = cv2.resize(pred_img, (28, 28))
-                    # pred_img = pred_img.reshape(1, 28, 28, 1)
+                    # pred_img = pred_img / 255.0
+                    # pred_img = pred_img.reshape(1, 784)
                     # prediction = mapping[model.predict_classes(pred_img)[0]]
+
+                    roi = gray_img[y-50:y+h+50, x-50:x+w+50]
+                    cv2.imwrite(output_path_img_temp, roi)
+                    cropped_img = cv2.imread(output_path_img_temp, 0)
+                    blur = cv2.GaussianBlur(cropped_img, (5, 5), 0)
+                    ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                    th3 = cv2.subtract(255, th3)
+                    pred_img = th3
+                    pred_img = cv2.resize(pred_img, (28, 28))
+                    pred_img = pred_img.reshape(1, 28, 28, 1)
+                    prediction = mapping[model.predict_classes(pred_img)[0]]
 
                     res_read = res_read + prediction
 
